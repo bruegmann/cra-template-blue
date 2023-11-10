2a46df051014547c60cd732e2d012515849a4dda
lgk-bsw <lgk@patorg.de>
2023-11-10 11:25:10 +0100

    Improved

--- template/src/pages/HomePage.tsx
+++ template/src/pages/HomePage.tsx
@@ -1,7 +1,6 @@
 import { Page, Header, HeaderTitle, Body } from "blue-react"
 
 import { appLogo, appTitle, getPhrase } from "../shared"
-import styles from "./HomePage.module.scss"
 
 export default function HomePage() {
     return (
@@ -13,38 +12,26 @@ export default function HomePage() {
             </Header>
 
             <Body containerClass="container">
-                <h1 className="mt-4 mb-3">Yay, your new app is ready! What now?</h1>
+                <h1 className="page-header mt-4 mb-3">Yay, your new app is ready! What now?</h1>
 
-                <p>
-                    Check out the{" "}
-                    <a href="https://bruegmann.github.io/blue-react/" target="_blank" rel="noopener noreferrer">
-                        docs
-                    </a>{" "}
-                    to find out more about the utilities and classes.
-                    <br />
-                    Under{" "}
-                    <a href="https://bruegmann.github.io/blue-react/recipes" target="_blank" rel="noopener noreferrer">
-                        Recipes
-                    </a>{" "}
-                    you can find some nice guides.
-                </p>
-
-                <div
-                    className="bg-theme text-white p-5 rounded-3 text-center"
-                    style={{ boxShadow: "var(--neu-shadow-1), var(--neu-shadow-inset-3)" }}
-                >
-                    <a
-                        className={`${styles.customNeuBtn} btn btn-primary btn-lg my-5`}
-                        href="https://bruegmann.github.io/blue-react/neu"
-                        target="_blank"
-                        rel="noopener noreferrer"
-                        style={{
-                            boxShadow: `var(--neu-shadow-3), var(--neu-shadow-inset-switch-1)`
-                        }}
-                    >
-                        Check out the Neumorphism utilities!
-                    </a>
-                </div>
+                <ol>
+                    <li>
+                        Remove this placeholder content by editing <code>./src/pages/HomePage.tsx</code>.
+                    </li>
+                    <li>
+                        Register more pages in <code>./src/App.tsx</code>.
+                    </li>
+                    <li>
+                        Take a look at the stylesheets in <code>./src/styles</code>.
+                    </li>
+                    <li>
+                        <a href="https://bruegmann.github.io/blue-react/" target="_blank" rel="noopener noreferrer">
+                            Check out the docs
+                        </a>{" "}
+                        to find out more about the components, utilities and classes. You'll also find many examples
+                        there.
+                    </li>
+                </ol>
             </Body>
         </Page>
     )
--- template/src/shared.ts
+++ template/src/shared.ts
@@ -1,27 +1,25 @@
 export const appLogo = "/logo512.png"
 export const appTitle = "My App Title"
 
-interface IPhrases {
-    [key: string]: string[]
+export const phrases = {
+    HELLO_WORLD: ["Hello World!", "Hallo Welt!"]
 }
 
-export const phrases: IPhrases = {
-    "HELLO_WORLD": ["Hello World!", "Hallo Welt!"]
-}
-
-export function getPhrase(keyword: string, countryCode: string | undefined = undefined, _phrases: IPhrases | undefined = undefined) {
-    countryCode = countryCode || (navigator.language).toLowerCase().indexOf("de") > -1 ? "de-DE" : "en-US"
+export function getPhrase(
+    keyword: keyof typeof phrases | (string & {}),
+    countryCode: string | undefined = undefined,
+    _phrases: { [key: string]: string[] } | undefined = undefined
+) {
+    countryCode = countryCode || navigator.language.toLowerCase().indexOf("de") > -1 ? "de-DE" : "en-US"
     _phrases = _phrases || phrases
 
     if (_phrases[keyword]) {
         if (countryCode.indexOf("de-") > -1) {
             return _phrases[keyword][1]
-        }
-        else {
+        } else {
             return _phrases[keyword][0]
         }
-    }
-    else {
+    } else {
         return keyword
     }
 }
